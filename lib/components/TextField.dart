bc67e53f201c84e07b021feec679feb1542bb2ab
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-09-23 15:04:45 +0100

    textfield component

--- lib/components/TextField.dart
+++ lib/components/TextField.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 
 class myTextField extends StatelessWidget {
-  const myTextField({super.key});
+  final TextEditingController? myController;
+  final bool obscureText;
+  final FormFieldValidator<String>? validator;
+  final String? labelText;
+  const myTextField({super.key, this.myController, required this.obscureText, this.validator, this.labelText});
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      controller: myController,
+      obscureText: obscureText,
 
     );
   }
