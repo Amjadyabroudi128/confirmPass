7840d6f9ad2316f64fa48717d91400926494a8fb
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-09-23 15:07:33 +0100

    textfield component

--- lib/components/TextField.dart
+++ lib/components/TextField.dart
@@ -4,15 +4,17 @@ class myTextField extends StatelessWidget {
   final TextEditingController? myController;
   final bool obscureText;
   final FormFieldValidator<String>? validator;
-  final String? labelText;
-  const myTextField({super.key, this.myController, required this.obscureText, this.validator, this.labelText});
+  final Widget? label;
+  const myTextField({super.key, this.myController, required this.obscureText, this.validator, this.label});
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       controller: myController,
       obscureText: obscureText,
-
+      decoration: InputDecoration(
+        label: label,
+      ),
     );
   }
 }
