93597fbc008e81306e8ddb543cc33e7c8ff308cf
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-09-24 13:03:52 +0100

    made focus border global

--- lib/Themes/Themes.dart
+++ lib/Themes/Themes.dart
@@ -1,8 +1,15 @@
 
  import 'package:flutter/material.dart';
 
+import '../consts.dart';
+
 class MyTheme {
   static final themes = ThemeData().copyWith(
-
+    inputDecorationTheme: InputDecorationTheme(
+      border: OutlineInputBorder(
+        borderSide: BorderSide(width: 1,color: borders),
+        borderRadius: BorderRadius.circular(18.0),
+      )
+    )
   );
  }
