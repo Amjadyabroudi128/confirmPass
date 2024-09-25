72647a17277e189dfc04a1c85abe771dbe0bf3e5
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-09-25 13:03:52 +0100

    border

--- lib/Themes/Themes.dart
+++ lib/Themes/Themes.dart
@@ -1,24 +1,22 @@
 
- import 'package:flutter/material.dart';
+ import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 import '../consts.dart';
 
 class MyTheme {
+  static final InputBorder _border = OutlineInputBorder(
+    borderSide: BorderSide(width: 1,color: borders),
+    borderRadius: BorderRadius.circular(18.0),
+  );
   static final themes = ThemeData().copyWith(
     inputDecorationTheme: InputDecorationTheme(
       floatingLabelStyle: TextStyle(
         color: floatingStyle,
         fontSize: 25
       ),
-      border: OutlineInputBorder(
-        borderSide: BorderSide(width: 1,color: borders),
-        borderRadius: BorderRadius.circular(18.0),
-      ),
-      focusedBorder: OutlineInputBorder(
-        borderSide: BorderSide(width: 1,
-            color: borders), //<-- SEE HERE
-        borderRadius: BorderRadius.circular(18.0),
-      ),
+      border: _border,
+      focusedBorder: _border
     )
   );
  }
