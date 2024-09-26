0ea0ccc1f4760931e45221798b749dbcc95618c1
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-09-26 14:16:05 +0100

    validation consts

--- lib/consts.dart
+++ lib/consts.dart
@@ -2,6 +2,9 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-
+// colors
 Color borders = Colors.black;
 Color floatingStyle = Colors.blue;
+// validate
+ String empty = "enter a password please";
+ String notMatching = "passwords do not match";
