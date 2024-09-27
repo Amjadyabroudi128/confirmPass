b76a4246be113450dcbe6e3392556bfead01b4c7
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-09-27 11:34:19 +0100

    password

--- lib/homePage/widgets/passwords.dart
+++ lib/homePage/widgets/passwords.dart
@@ -1,10 +1,8 @@
-import 'package:confirm_pass/consts.dart';
 import 'package:flutter/material.dart';
-
 import '../../components/TextField.dart';
 
-class password extends StatefulWidget {
-  const password({
+class Password extends StatefulWidget {
+  const Password({
     super.key,
     required this.pass,
   });
@@ -12,39 +10,42 @@ class password extends StatefulWidget {
   final TextEditingController pass;
 
   @override
-  State<password> createState() => _passwordState();
+  State<Password> createState() => _PasswordState();
 }
 
-class _passwordState extends State<password> {
+class _PasswordState extends State<Password> {
   bool isHidden = true;
+
   void _togglePasswordView() {
     setState(() {
       isHidden = !isHidden;
     });
   }
+
   @override
   Widget build(BuildContext context) {
-    String pass = "Password";
+    String labelText = "Password";
+
     return myTextField(
       obscureText: isHidden,
       suffix: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: IconButton(onPressed: (){
-          setState(() {
-            _togglePasswordView();
-          });
-        }, icon: isHidden ? Text("show") : Text("hide")
+        child: IconButton(
+          onPressed: _togglePasswordView,
+          icon: isHidden ? Text("show") : Text("hide"),
         ),
       ),
       myController: widget.pass,
-      label: Text(pass),
-      validator: (value){
-        if ( value == null || widget.pass.text.isEmpty) {
-          return empty;
+      label: labelText, // Label for password
+      validator: (value) {
+        if (value == null || value.isEmpty) { // Validate the value, not the label
+          return 'Please enter password';
         }
-        return null;
+        if (value.length < 6) {
+          return 'Password must be at least 6 characters long';
+        }
+        return null; // Valid password
       },
     );
   }
 }
-
