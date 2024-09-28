3fcc7be7d89a0c32e93f24465da428d8a0d7d877
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-09-28 14:05:41 +0100

    static border radios

--- lib/Themes/Themes.dart
+++ lib/Themes/Themes.dart
@@ -10,7 +10,7 @@ class MyTheme {
  static final BorderRadius myBorder = BorderRadius.circular(18);
   static final _border = OutlineInputBorder(
     borderSide: BorderSide(width: 1,color: borders),
-    borderRadius: BorderRadius.circular(18.0),
+    borderRadius: myBorder
   );
   static final OutlineInputBorder _error = OutlineInputBorder(
       borderSide: BorderSide(color: Colors.red, width: 2),
