882384b91a99e9760e9a4f1a4dd710f9e639514f
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-09-28 13:58:46 +0100

    error focus border

--- lib/Themes/Themes.dart
+++ lib/Themes/Themes.dart
@@ -1,16 +1,19 @@
 
  import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 import '../consts.dart';
 
 class MyTheme {
+
   static final _border = OutlineInputBorder(
     borderSide: BorderSide(width: 1,color: borders),
     borderRadius: BorderRadius.circular(18.0),
   );
   static final _error = OutlineInputBorder(
       borderSide: BorderSide(color: Colors.red, width: 2),
+    borderRadius: BorderRadius.circular(18.0),
   );
   static final themes = ThemeData().copyWith(
     inputDecorationTheme: InputDecorationTheme(
