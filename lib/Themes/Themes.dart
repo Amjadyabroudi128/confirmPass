72138ac17ae1df79a7f8866d6a996db6a7f03f07
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-09-29 14:31:38 +0100

    unused imports

--- lib/Themes/Themes.dart
+++ lib/Themes/Themes.dart
@@ -1,9 +1,4 @@
-
- import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
-import 'package:flutter/rendering.dart';
-
 import '../consts.dart';
 
 class MyTheme {
