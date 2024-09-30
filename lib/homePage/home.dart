b6d1b4e5a4b1bcb55737e8d7510497dd704a6adb
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-09-30 12:38:23 +0100

    elevatedBtn widget

--- lib/homePage/home.dart
+++ lib/homePage/home.dart
@@ -2,6 +2,7 @@ import 'package:confirm_pass/components/elevatedBtn.dart';
 import 'package:confirm_pass/components/snackBart.dart';
 import 'package:confirm_pass/homePage/widgets/confirmingPass.dart';
 import 'package:confirm_pass/homePage/widgets/passwords.dart';
+import 'package:confirm_pass/homePage/widgets/submitBtn.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -53,48 +54,6 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class submitBtn extends StatelessWidget {
-  const submitBtn({
-    super.key,
-    required GlobalKey<FormState> formKey,
-    required this.confirmPass,
-    required this.pass,
-  }) : _formKey = formKey;
-
-  final GlobalKey<FormState> _formKey;
-  final TextEditingController confirmPass;
-  final TextEditingController pass;
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 40,
-      width: 130,
-      child: kElevatedButton(
-        onPressed: () {
-          if (_formKey.currentState!.validate()) {
-            showSnackbar(context, "password is Valid");
-          } else if (confirmPass.text != pass.text) {
-            showSnackbar(context, "Passwords do not match");
-          }
-        },
-        child: Text("Submit"),
-        style: ElevatedButton.styleFrom(
-          elevation: 5,
-          backgroundColor: Colors.black,
-            foregroundColor: Colors.white,
-            textStyle: TextStyle(
-              fontSize: 20
-          ),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12)
-          )
         ),
       ),
     );
