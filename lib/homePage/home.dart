b8497a56ef4ae985c0a70fee6876ea63c9691927
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-09-30 12:37:53 +0100

    elevatedBtn widget

--- lib/homePage/home.dart
+++ lib/homePage/home.dart
@@ -47,36 +47,54 @@ class _MyHomePageState extends State<MyHomePage> {
                   Password(pass: pass),
                   SizedBox(height: 20),
                   ConfirmingPass(confirmPass: confirmPass, pass: pass),
-                  SizedBox(height: 19),
-                  SizedBox(
-                    height: 40,
-                    width: 130,
-                    child: kElevatedButton(
-                      onPressed: () {
-                        if (_formKey.currentState!.validate()) {
-                          showSnackbar(context, "password is Valid");
-                        } else if (confirmPass.text != pass.text) {
-                          showSnackbar(context, "Passwords do not match");
-                        }
-                      },
-                      child: Text("Submit"),
-                      style: ElevatedButton.styleFrom(
-                        elevation: 5,
-                        backgroundColor: Colors.black,
-                          foregroundColor: Colors.white,
-                          textStyle: TextStyle(
-                            fontSize: 20
-                        ),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12)
-                        )
-                      ),
-                    ),
-                  ),
+                  SizedBox(height: 22),
+                  submitBtn(formKey: _formKey, confirmPass: confirmPass, pass: pass),
                 ],
               ),
             ),
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class submitBtn extends StatelessWidget {
+  const submitBtn({
+    super.key,
+    required GlobalKey<FormState> formKey,
+    required this.confirmPass,
+    required this.pass,
+  }) : _formKey = formKey;
+
+  final GlobalKey<FormState> _formKey;
+  final TextEditingController confirmPass;
+  final TextEditingController pass;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 40,
+      width: 130,
+      child: kElevatedButton(
+        onPressed: () {
+          if (_formKey.currentState!.validate()) {
+            showSnackbar(context, "password is Valid");
+          } else if (confirmPass.text != pass.text) {
+            showSnackbar(context, "Passwords do not match");
+          }
+        },
+        child: Text("Submit"),
+        style: ElevatedButton.styleFrom(
+          elevation: 5,
+          backgroundColor: Colors.black,
+            foregroundColor: Colors.white,
+            textStyle: TextStyle(
+              fontSize: 20
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12)
+          )
         ),
       ),
     );
