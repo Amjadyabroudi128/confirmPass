e56623e05f98e822ee9603d32cb24147942ec7ff
Amjadyabroudi128 <amjadyabroudi.20@gmail.com>
2024-10-11 12:41:04 +0100

    confirm pass

--- lib/homePage/widgets/confirmingPass.dart
+++ lib/homePage/widgets/confirmingPass.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import '../../components/TextField.dart';
 import '../../consts.dart';
 
-class ConfirmingPass extends StatelessWidget {
+class ConfirmingPass extends StatefulWidget {
   const ConfirmingPass({
     super.key,
     required this.confirmPass,
@@ -13,16 +13,43 @@ class ConfirmingPass extends StatelessWidget {
   final TextEditingController pass;
 
   @override
+  State<ConfirmingPass> createState() => _ConfirmingPassState();
+}
+
+class _ConfirmingPassState extends State<ConfirmingPass> {
+  @override
+  void initState() {
+    super.initState();
+    // Listen for changes in both password fields to update UI accordingly
+    widget.pass.addListener(() {
+      setState(() {});
+    });
+    widget.confirmPass.addListener(() {
+      setState(() {});
+    });
+  }
+
+  bool passwordsMatch() {
+    return widget.pass.text == widget.confirmPass.text && widget.pass.text.isNotEmpty;
+  }
+
+  @override
   Widget build(BuildContext context) {
     return myTextField(
       obscureText: true,
-      myController: confirmPass,
+      myController: widget.confirmPass,
       label: confirm,
+      suffix: Padding(
+        padding: const EdgeInsets.only(right: 8),
+        child: passwordsMatch()
+            ? const Icon(Icons.check, color: Colors.green) // Show tick icon
+            : const Icon(Icons.clear, color: Colors.red), // Show clear icon
+      ),
       validator: (value) {
         if (value == null || value.isEmpty) {
           return enter;
         }
-        if (value != pass.text) {
+        if (value != widget.pass.text) {
           return notMatching;
         }
         return null; // Passwords match
